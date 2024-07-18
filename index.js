ee7f4189c50d8a9dd758ab0e0f713f8c52546560
Alexs Ruiz <ralexs.acu@gmail.com>
2024-07-18 16:48:52 -0600

    Add nodemon dependency

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
 const app = express()
 
-
-
 app.get('/', (req, res) => {
   console.log('Hello from /');
-  res.send("hello world")
+  res.send("hello world!!!!!")
 })
-
 
 app.listen(5000, () => {
   console.log('Running on port 5000');
 })
-
