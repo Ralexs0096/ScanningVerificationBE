7589cf45e9c15fd94d79820e85f5a08e44299c3d
Alexs Ruiz <ralexs.acu@gmail.com>
2024-07-18 16:45:16 -0600

    Server listening

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from 'express';
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+  console.log('Hello from /');
+  res.send("hello world")
+})
+
+
+app.listen(5000, () => {
+  console.log('Running on port 5000');
+})
+
