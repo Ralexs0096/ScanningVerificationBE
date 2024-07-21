b4af8d8cf80eb2de3ab4669e9aa604cda4c1a8b6
Alexs Ruiz <ralexs.acu@gmail.com>
2024-07-21 14:20:56 -0600

    Add the db connection to mysql
    - new endpoint to test the connection

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+import mysql from 'mysql2/promise';
 import express from 'express';
 const app = express()
 
-app.get('/', (req, res) => {
-  console.log('Hello from /');
-  res.send("hello world!!!!!")
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
+});
+
+app.get('/', async (req, res) => {
+  // A simple SELECT query
+  try {
+    const [results] = await connection.query(
+      'SELECT * FROM `tb_area`'
+    );
+
+    res.json({
+      results
+    })
+
+  } catch (err) {
+    console.log(err);
+  }
 })
 
-app.listen(5000, () => {
+
+
+app.listen(process.env.SERVER_PORT, () => {
   console.log('Running on port 5000');
 })
