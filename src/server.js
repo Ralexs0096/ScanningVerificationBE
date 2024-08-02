46585162e82fc42a1acabadbb2556a143b626afb
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-01 23:42:30 -0600

    Delegate db connection to the Server class

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from 'express'
+import { createConnection } from './config/db/connection.js'
 
 class Server {
   constructor() {
     this.app = express()
     this.port = process.env.SERVER_PORT ?? 5000
+
+    this.dbConnection()
   }
 
-  dbConnection() {
-
+  async dbConnection() {
+    await createConnection()
+    console.log("Database is connected...");
   }
 
   middleware() {
