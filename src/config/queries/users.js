84ea3a40021bc572f46b4aaf927495a5cad1c518
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-04 16:28:51 -0600

    Add new query to get a user by id (codigo empleado)

--- src/config/queries/users.js
+++ src/config/queries/users.js
@@ -1,2 +1,4 @@
 
 export const getAllUsersQuery = `SELECT * FROM tb_maestro_empleados tme`
+
+export const getUserById = (id) => `SELECT * FROM tb_maestro_empleados tme WHERE tme.codigo_emp = ${id}`
