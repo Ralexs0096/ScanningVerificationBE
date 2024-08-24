b3f9b4d86c226ce2625e13bb70e15e85c1293327
Alexs Ruiz <ralexs.acu@gmail.com>
2024-08-23 23:47:52 -0600

    Adjust the query to get the user by codigoEmp and codigoEc

--- src/config/queries/users.js
+++ src/config/queries/users.js
@@ -1,4 +1,7 @@
 
 export const getAllUsersQuery = `SELECT * FROM tb_maestro_empleados tme`
 
-export const getUserByIdQuery = (id) => `SELECT * FROM tb_maestro_empleados tme WHERE tme.codigo_emp = ${id}`
+export const getUserByIdQuery = (codigoEmp, codigoEc) => `
+SELECT nombre_completo, cedula_id FROM tb_maestro_empleados tme 
+WHERE tme.codigo_emp = ${codigoEmp} and tme.codigo_ec = ${codigoEc}
+`
